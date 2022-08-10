20a15be1b4a4456afc5302215b5405b39d798068
lorenzo3117 <lorenzocatalano37@gmail.com>
2022-08-10 17:31:11 +0200

    Mobile responsiveness

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,23 +1,34 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { NAV } from "../../constants/nav";
 import { LangSelector } from "../lang-selector/LangSelector";
+import styles from "./Nav.module.scss";
 
 export const Nav = () => {
   const { t } = useTranslation("nav");
 
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+  };
+
+  window.addEventListener("resize", handleResize);
+
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <strong>Lorenzo Catalano</strong>
         </li>
       </ul>
       <ul>
-        {NAV.map((nav, index) => (
-          <li key={index}>
-            <a href={`#${nav}`}>{t(nav)}</a>
-          </li>
-        ))}
+        {width >= 600 &&
+          NAV.map((nav, index) => (
+            <li key={index}>
+              <a href={`#${nav}`}>{t(nav)}</a>
+            </li>
+          ))}
         <li>
           <LangSelector />
         </li>
